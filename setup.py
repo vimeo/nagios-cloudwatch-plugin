cc4973afe31a0751b1a10e51cc69b759312b24ee
William Hutson <wilrnh@msn.com>
2013-09-23 20:08:25 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nagios-cloudwatch-plugin',
+        version='0.1',
+        description='Nagios Plugin for AWS Cloudwatch Metrics',
+        author='William Hutson',
+        author_email='wilrnh@gmail.com',
+        license="MIT",
+        keywords="nagios cloudwatch plugin",
+        url='https://github.com/FastSociety/nagios-cloudwatch-plugin',
+        install_requires=["argparse","nagiosplugin","boto"],
+)
