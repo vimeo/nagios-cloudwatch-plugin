08797c6b52eb43a78fa5bb8a3731b989a581b436
Soenke Ruempler <soenke@jimdo.com>
2013-10-24 02:34:13 +0200

    fix trailing whitespace

--- check_cloudwatch.py
+++ check_cloudwatch.py
@@ -4,56 +4,56 @@ import argparse, logging, nagiosplugin, boto
 from datetime import datetime, timedelta
 
 class CloudWatchMetric(nagiosplugin.Resource):
-    
+
     def __init__(self, namespace, metric, dimensions, statistic):
         self.namespace = namespace
         self.metric=metric
         self.dimensions = dimensions
         self.statistic = statistic
-        
+
     def probe(self):
         logging.info('getting stats from cloudwatch')
         cw = boto.connect_cloudwatch()
         start_time = datetime.utcnow() - timedelta(minutes=1)
         end_time = datetime.utcnow()
         stats = []
-        stats = cw.get_metric_statistics(60, start_time, end_time, 
+        stats = cw.get_metric_statistics(60, start_time, end_time,
                                          self.metric, self.namespace, self.statistic, self.dimensions)
         if len(stats) == 0:
             return []
-        
+
         stat = stats[0]
         return [nagiosplugin.Metric('cloudwatchmetric', stat[self.statistic], stat['Unit'])]
-        
+
 class CloudWatchMetricSummary(nagiosplugin.Summary):
-    
+
     def __init__(self, namespace, metric, dimensions, statistic):
         self.namespace = namespace
         self.metric=metric
         self.dimensions = dimensions
         self.statistic = statistic
-        
+
     def ok(self, results):
         full_metric = '%s:%s' % (self.namespace, self.metric)
         return 'CloudWatch Metric %s with dimenstions %s' % (full_metric, self.dimensions)
-        
+
     def problem(self, results):
         full_metric = '%s:%s' % (self.namespace, self.metric)
         return 'CloudWatch Metric %s with dimenstions %s' % (full_metric, self.dimensions)
-    
+
 class KeyValArgs(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         kvs = {}
         for pair in values.split(','):
             kv = pair.split('=')
             kvs[kv[0]] = kv[1]
-        setattr(namespace, self.dest, kvs)      
+        setattr(namespace, self.dest, kvs)
 
 @nagiosplugin.guarded
 def main():
-    
+
     argp = argparse.ArgumentParser(description='Nagios plugin to check cloudwatch metrics')
-    
+
     argp.add_argument('-n', '--namespace', required=True,
                       help='namespace for cloudwatch metric')
     argp.add_argument('-m', '--metric', required=True,
@@ -70,9 +70,9 @@ def main():
                       help='increase verbosity (use up to 3 times)')
     argp.add_argument('-t', '--timeout', default=10,
                       help='abort execution after TIMEOUT seconds')
-    
+
     args=argp.parse_args()
-    
+
     check = nagiosplugin.Check(
             CloudWatchMetric(args.namespace, args.metric, args.dimensions, args.statistic),
             nagiosplugin.ScalarContext('cloudwatchmetric', args.warning, args.critical),
