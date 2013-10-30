32dd6ae8bc18f38b070ad0c3ea1b55164071e227
William Hutson <wilrnh@msn.com>
2013-10-30 17:04:50 -0400

    Bumped version to 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='nagios-cloudwatch-plugin',
-        version='0.2',
+        version='0.2.1',
         author='William Hutson',
         author_email='wilrnh@gmail.com',
         license="MIT",
